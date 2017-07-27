cd3140fa77239f5c3aa51bd1470e472e4f609954
Nikki Everett <nikki.everett@outlook.com>
2017-07-27 10:35:09 -0500

    restructured files for deployment test 1

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
 const port = process.env.PORT || 5003;
 
-app.use(express.static(path.resolve(__dirname, '/public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.listen(port, function(){
   console.log('listening on port ', port);
