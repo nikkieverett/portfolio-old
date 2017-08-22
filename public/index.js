14c8ee8975f0e90393c87d305e062c6da964d14c
Nikki Everett <nikki.everett@outlook.com>
2017-08-22 12:16:10 -0500

    minor css changes

--- public/index.js
+++ public/index.js
@@ -39,3 +39,37 @@ function slideDown(e) {
     // This timeout length should match the CSS animation time (.8s).
   }, 800);
 }
+function slideUp(e) {
+
+  // Delegate.
+  if (e.target.className != 'pre') {
+    return;
+  }
+
+  // Prevent firing simultaneously.
+  page.onclick = '';
+  self = e.target.parentNode;
+  var offset = self.getBoundingClientRect();
+  console.log(offset);
+  var scroll = self.offsetTop+150;
+  console.log(scroll);
+
+  // CSS Transition slide.
+  page.style.top = (-offset.height-150-offset.top)+ 'px';
+    console.log(page.style.top);
+
+    // console.log('offset.top', offset.top, 'offset.height', offset.height, 'offsetTop', self.offsetTop);
+
+  setTimeout(function () {
+    // let position = offset.height-150;
+    // Reposition the real scrollbar.
+    page.style.transition = 'none';
+    page.style.top = '';
+    window.scrollTo(0, offset.height+scroll);
+    page.style.transition = transition;
+    // Reattach event.
+    page.onclick = slideDown;
+
+    // This timeout length should match the CSS animation time (.8s).
+  }, 800);
+}
